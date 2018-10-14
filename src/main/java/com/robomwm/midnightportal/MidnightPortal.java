cf9f7e0f1352034dffd9860d670426051f056f90
RoboMWM <robomwm@users.noreply.github.com>
2018-10-14 10:51:54 -0700

    Create the manager
    Initially was going to do something similar to what I do except I have
    to look for a different type of portal, so using the Bukkit TravelAgent
    isn't going to work. Also, since we're always using a multiplier, we
    don't really need to store locations, so that file is also probably
    going away. So the fun part is doing what the TravelAgent does for these
    new portals...

---
+++ src/main/java/com/robomwm/midnightportal/MidnightPortal.java
@@ -0,0 +1,13 @@
+package com.robomwm.midnightportal;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created on 10/10/2018.
+ *
+ * @author RoboMWM
+ */
+public class MidnightPortal extends JavaPlugin
+{
+
+}
